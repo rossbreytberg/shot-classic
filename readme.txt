dca802af113eff4f98095294406a21cea4c072fe
rossbreytberg <rossbreytberg@gmail.com>
2011-11-16 03:43:26 -0800

    v0.2

---
+++ readme.txt
@@ -0,0 +1,24 @@
+Shot is a shooting game.
+
+Click on the red square as fast as possible to score the most points in 30 seconds.
+There is a high score board displayed at the end.
+
+
+
+
+Requirements:
+web.py
+mysql
+
+
+
+
+Version History:
+
+v0.2:
+-points per hit vary based on how fast you click
+-points earned per hit are shown as a yellow number that fades away
+-visual improvements
+
+v0.1:
+-first release
--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,54 @@
- /*$(document).everyTime(200, function(){$("#target").animate({top: Math.random()*718, left: Math.random()*974}, 200)}, 150000)*/
-
- $(document).ready(function(){
+$(document).ready(function(){
+  // hide divs that shouldn't be visible
   $("#scoresform").hide()
-  $("#target").stop().animate({top: Math.random()*550, left: Math.random()*750}, 0)
-  $("#target").hide()
   $("#highscores").hide()
+
+  //initialize variables
   var score = 0
+  var tempscore = 100
   var time = 30
+  var mousex = 0
+  var mousey = 0
+
+  //game starts
   $("#start").click(function(){
     $("#start").hide(0, function(){
-        $("#target").show()
-        $("#target").click(function(){
-          $("#target").stop().animate({top: Math.random()*550, left: Math.random()*750}, 200)
-          score += 50
-          $("#score").html(score)
-          $("#sound").html("<audio autoplay='autoplay'><source src='static/sounds/ding.wav' type='audio/x-wav' /></audio>")})
 
+        $("#game").css('cursor', 'crosshair')
+        
+        //create target
+        jQuery('<div/>', {
+              id: 'target',
+          }).appendTo('#game').css('top', Math.random()*550).css('left', Math.random()*750)
+
+        //keep mouse position updated
+        $(game).mousemove(function(m){mousex = m.pageX; mousey = m.pageY})
+
+        //target hit
+        $("#target").click(function(){
+          $("#sound").html("<audio autoplay='autoplay'><source src='static/sounds/ding.wav' type='audio/x-wav' /></audio>")
+          $(this).stop()
+          $(document).stopTime('tempscore')
+          score += tempscore
+          //show points for earned for hit
+          jQuery('<div/>', {
+              class: 'tempscore',
+              text: tempscore
+          }).appendTo('#game').css('top', mousey).css('left', mousex).animate({top: '-=10'}, 200).fadeOut(200, function(){$(this).remove()})
+          $("#score").html(score)
+          tempscore = 100
+          $(document).everyTime(30, 'tempscore', function(){if (tempscore > 1) {tempscore -= 1}})
+          $(this).animate({top: Math.random()*550, left: Math.random()*750}, 200)
+        })
+
+        //game timer
         $(document).everyTime(1000, function(){
           $("#time").html($("#time").text() - 1) 
           time -= 1
+          //game ends
           if (time == 0) {
-            $("#target").hide()
+            $("#target").remove()
+            $("#game").css('cursor', 'auto')
             $("#scoreval").html("<input type='hidden' name='score' min="+score+" max="+score+" value="+score+" />")
             $("#scoresform").show()
             $("#highscores").show()
